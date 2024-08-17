1d5dfc3e4d1d68307a7498993863a362fa5c719c
Doddy Adi Pranatha <doddy.pranatha@flexidev.co>
2024-08-17 14:01:01 +0800

    initiate project

---
+++ Interpreter/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.IO;
+using System.Text;
+
+namespace Interpreter
+{
+    internal class Program
+    {
+        private static bool hadError = false;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        private static void RunFile(string path)
+        {
+            byte[] bytes = File.ReadAllBytes(path);
+            string content = Encoding.Default.GetString(bytes);
+
+            Run(content);
+        }
+
+        private static void RunPrompt()
+        {
+            using (var reader = new StreamReader(Console.OpenStandardInput()))
+            {
+                while (true)
+                {
+                    Console.Write("> ");
+
+                    string line = reader.ReadLine();
+                    if (line == null) break;
+                    Run(line);
+                    hadError = true;
+
+                }
+            }
+        }
+
+        private static void Run(string source)
+        {
+
+        }
+
+        private static void Error(int line, string message) { 
+            Report(line, "", message);  
+        }
+
+        private static void Report(int line, string where, string message) { 
+            Console.WriteLine($"[line {line}] Error {where} : {message}");
+            hadError = true;
+        }
+    }
+}
