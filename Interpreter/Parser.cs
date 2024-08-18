c49bd338b9de577a9ddc81a8153b6c4a835ca4f5
Doddy Adi Pranatha <doddy.pranatha@flexidev.co>
2024-08-18 13:24:30 +0800

    Add parser class

---
+++ Interpreter/Parser.cs
@@ -0,0 +1,52 @@
+﻿namespace Interpreter
+{
+    internal class Parser
+    {
+        private readonly List<Token> _tokens;
+        private int _current = 0;
+
+        public Parser(List<Token> tokens) 
+        { 
+            this._tokens = tokens;
+        }
+
+
+        private bool Match(params TokenType[] types) {
+            foreach (var type in types) {
+                if (Check(type))
+                {
+                    Advance();
+                    return true;
+                }
+            }
+
+            return false;  
+        }
+
+        private bool Check(TokenType type)
+        {
+            if(IsAtEnd()) return false;
+            return Peek().Type == type;
+        }
+
+        private Token Advance() {
+            if (!IsAtEnd()) _current++;
+            return Previous();
+        }
+
+        private bool IsAtEnd()
+        {
+            return Peek().Type == TokenType.Eof;
+        }
+
+        private Token Peek()
+        {
+            return _tokens[_current];
+        }
+
+        private Token Previous()
+        {
+            return _tokens[_current - 1];
+        }
+    }
+}
