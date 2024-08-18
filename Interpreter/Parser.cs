f5b123983d7e0f17cd446cc6034ffc05b0d72367
Doddy Adi Pranatha <doddy.pranatha@flexidev.co>
2024-08-18 20:07:46 +0800

    Add parser class

--- Interpreter/Parser.cs
+++ Interpreter/Parser.cs
@@ -5,11 +5,117 @@
         private readonly List<Token> _tokens;
         private int _current = 0;
 
+        private  class ParseError : Exception
+        {
+
+        } 
+
         public Parser(List<Token> tokens) 
         { 
             this._tokens = tokens;
         }
 
+        public Expression Parse()
+        {
+            try
+            {
+                return Expression();
+            }
+            catch (ParseError error)
+            {
+                Synchronize();
+                return null;
+            }
+        }
+
+        private Expression Expression() {
+            return Equality();
+        }
+
+        private Expression Equality()
+        {
+            Expression expr = Comparison();
+
+            while (Match(TokenType.BangEqual, TokenType.EqualEqual))
+            {
+                Token @operator = Previous();
+                Expression right = Comparison();
+                expr = new Expression.Binary(expr, @operator, right);
+            }
+
+            return expr;
+        }
+
+        private Expression Comparison()
+        {
+            Expression expr = Term();
+
+            while (Match(TokenType.Greater, TokenType.GreaterEqual, TokenType.Less, TokenType.LessEqual))
+            {
+                Token @operator = Previous();
+                Expression right = Term();
+                expr = new Expression.Binary(expr, @operator, right);
+            }
+
+            return expr;
+        }
+
+        private Expression Term() {
+            Expression expr = Factor();
+
+            while (Match(TokenType.Minus, TokenType.Plus))
+            {
+                Token @operator = Previous();
+                Expression right = Factor();
+                expr = new Expression.Binary(expr, @operator, right);
+            }
+
+            return expr;
+        }
+
+        private Expression Factor() {
+            Expression expr = Unary();
+
+            while (Match(TokenType.Slash, TokenType.Star))
+            {
+                Token @operator = Previous();
+                Expression right = Unary();
+                expr = new Expression.Binary(expr, @operator, right);
+            }
+
+            return expr;
+        }
+
+        private Expression Unary() {
+            if (Match(TokenType.Bang, TokenType.Minus))
+            {
+                Token @operator = Previous();
+                Expression right = Unary();
+                return new Expression.Unary(@operator, right);
+            }
+
+            return Primary();
+        }
+
+        private Expression Primary() {
+            if(Match(TokenType.False)) return new Expression.Literal(false);
+            if(Match(TokenType.True)) return new Expression.Literal(true);
+            if(Match(TokenType.Nil)) return new Expression.Literal(null);
+
+            if(Match(TokenType.Number, TokenType.String))
+            {
+                return new Expression.Literal(Previous().Literal);
+            }
+
+            if (Match(TokenType.LeftParen))
+            {
+                Expression expression = Expression();
+                Consume(TokenType.RightParen, "Expect ')' after expression.");
+                return new Expression.Grouping(expression);
+            }
+
+            throw Error(Peek(), "Expect expression.");
+        }
 
         private bool Match(params TokenType[] types) {
             foreach (var type in types) {
@@ -47,6 +153,42 @@
         private Token Previous()
         {
             return _tokens[_current - 1];
+        }
+
+        private Token Consume(TokenType type, string message) { 
+            if(Check(type)) return Advance();
+
+            throw Error(Peek(),message);
+        }
+
+        private ParseError Error(Token token, string message) {
+            Program.Error(token, message);
+            return new ParseError();
+        }
+
+        private void Synchronize()
+        {
+            Advance();
+
+            while (!IsAtEnd())
+            {
+                if (Previous().Type == TokenType.Semicolon) return;
+
+                switch (Peek().Type)
+                {
+                    case TokenType.Class:
+                    case TokenType.Fun:
+                    case TokenType.Var:
+                    case TokenType.For:
+                    case TokenType.If:
+                    case TokenType.While:
+                    case TokenType.Print:
+                    case TokenType.Return:
+                        return;
+                }
+
+                Advance();
+            }
         }
     }
 }
--- Interpreter/Program.cs
+++ Interpreter/Program.cs
@@ -30,7 +30,7 @@ namespace Interpreter
                     string line = reader.ReadLine();
                     if (line == null) break;
                     Run(line);
-                    hadError = true;
+                    hadError = false;
 
                 }
             }
@@ -41,13 +41,33 @@ namespace Interpreter
             var scanner = new Scanner(source);
             var tokens = scanner.GenerateTokenList();
 
-            foreach (var token in tokens) { 
-                Console.WriteLine(token);
-            }
+            //foreach (var token in tokens) { 
+            //    Console.WriteLine(token);
+            //}
+
+            var parser = new Parser(tokens);
+            var expression = parser.Parse();
+
+            if (hadError) return;
+
+            var astPrinter = new AstPrinter();
+            Console.WriteLine(astPrinter.Print(expression));
         }
 
         public static void Error(int line, string message) { 
             Report(line, "", message);  
+        }
+
+        public static void Error(Token token, string message)
+        {
+            if (token.Type == TokenType.Eof)
+            {
+                Report(token.Line, " at end", message);
+            }
+            else
+            {
+                Report(token.Line, $" at '{token.Lexeme}'", message);
+            }
         }
 
         private static void Report(int line, string where, string message) { 
